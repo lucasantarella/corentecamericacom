cd3ffd7d8b8e2d5292dd0a496b88d1604ba4c774
Luca Santarella <luca.santarella@gmail.com>
2016-07-20 14:59:06 -0400

    Finished gulp integration

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,9 @@ var gulp = require('gulp'),
     tar = require('gulp-tar'),
     args = require('yargs').argv,
     gulpif = require('gulp-if'),
+    duration = require('gulp-duration'),
+    size = require('gulp-size'),
+    flatten = require('gulp-flatten'),
     minify = require('gulp-html-minifier');
 
 // Init dirs
@@ -28,6 +31,12 @@ var dirs = {
     },
     build: './build/'
 };
+
+/*
+ *
+ *  Build tasks
+ *
+ */
 
 // Sass task
 gulp.task('sass', function () {
@@ -55,10 +64,38 @@ gulp.task('js', function () {
 });
 
 // Vendor task
-gulp.task('vendor', function () {
+gulp.task('vendor', ['vendor-js', 'vendor-css']);
 
+// Vendor CSS
+gulp.task('vendor-css', ['vendor-sass'], function () {
+    gulp
+        .src(dirs.build + 'vendor/**/*.css')
+        .pipe(sourcemaps.init())
+        .pipe(concat('vendor.min.css'))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest(dirs.build + 'vendor/'))
+        .pipe(notify({
+            onLast: true,
+            message: "Vendors just got minified in style!"
+        }));
+});
 
-    // Vendor JS
+// Vendor SASS
+gulp.task('vendor-sass', function () {
+    gulp
+        .src(dirs.src.vendor + '**/*.{sass,scss}')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            noCache: true,
+            outputStyle: "compressed"
+        }))
+        .pipe(rename({dirname: dirs.build + 'vendor/'}))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest(dirs.build + 'vendor/'));
+});
+
+// Vendor JS
+gulp.task('vendor-js', function () {
     gulp
         .src(dirs.src.vendor + "**/*.js")
         .pipe(sourcemaps.init())
@@ -66,35 +103,23 @@ gulp.task('vendor', function () {
         .pipe(uglify())
         .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest(dirs.build + 'vendor/'));
-
-    // // Vendor SASS
-    // gulp
-    //     .src(dirs.src.vendor + '**/*.{sass,scss}')
-    //     .pipe(sourcemaps.init())
-    //     .pipe(concat('vendor.min.sass'))
-    //     .pipe(sass({
-    //         noCache: true,
-    //         outputStyle: "compressed"
-    //     }))
-    //     .pipe(sourcemaps.write('./'))
-    //     .pipe(gulp.dest(dirs.build + 'vendor/'))
-    //     .pipe(notify({
-    //         message: "Vendors just got super Sassy!"
-    //     }));
 });
 
 // Copy assets
-gulp.task('assets', function () {
-    gulp
+gulp.task('assets', ['img-assets', 'font-assets']);
+gulp.task('img-assets', function () {
+    return gulp
         .src(dirs.src.img + '**/*.*')
         .pipe(gulp.dest(dirs.build + 'img/'));
-    gulp
+});
+gulp.task('font-assets', function () {
+    return gulp
         .src(dirs.src.fonts + '**/*.*')
         .pipe(gulp.dest(dirs.build + 'fonts/'));
 });
 
 // Covert pug files
-gulp.task('pug', function buildHTML() {
+gulp.task('pug', function () {
     return gulp
         .src(dirs.src.main + '*.pug')
         .pipe(pug())
@@ -111,10 +136,10 @@ gulp.task('minify', ['pug'], function () {
 
 // Install all bower components
 gulp.task('bower', function () {
-    gulp
+    return gulp
         .src(dirs.src.main + 'bower.json')
-        .pipe(gulp.dest(dirs.build));
-    bower({cmd: 'update', directory: 'bower_components', cwd: dirs.build});
+        .pipe(gulp.dest(dirs.build))
+        .pipe(bower({cmd: 'update', directory: 'bower_components', cwd: dirs.build}));
 });
 
 // Build task
@@ -133,4 +158,94 @@ gulp.task('release', ['build'], function () {
             .pipe(gzip())
             .pipe(gulp.dest('.'));
     }
+});
+
+/*
+ *
+ *  Watch Tasks
+ *
+ */
+
+// Default watch
+gulp.task('default', function () {
+    gulp.watch(dirs.src.vendor + "**/*.{sass,scss,css}", ['watch-vendor-css']);
+    gulp.watch(dirs.src.vendor + "**/*.js", ['watch-vendor-js']);
+    gulp.watch(dirs.src.css + "**/*.{sass,scss,css}", ['watch-css']);
+    gulp.watch(dirs.src.js + "**/*.js", ['watch-js']);
+    gulp.watch(dirs.src.main + "*.pug", ['watch-pug']);
+});
+
+// Watch Vendor Sass task
+gulp.task('watch-vendor-sass', function () {
+    return gulp
+        .src(dirs.src.vendor + '**/*.{sass,scss}')
+        .pipe(sourcemaps.init())
+        .pipe(sass({}))
+        .pipe(flatten())
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest(dirs.src.vendor));
+});
+
+// Watch Vendor CSS task
+gulp.task('watch-vendor-css', ['watch-vendor-sass'], function () {
+    gulp
+        .src(dirs.src.vendor + '**/*.css')
+        .pipe(sourcemaps.init())
+        .pipe(concat("vendor.min.css"))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest(dirs.src.vendor));
+});
+
+
+// Watch Sass task
+gulp.task('watch-sass', function () {
+    gulp
+        .src(dirs.src.css + '**/*.{sass,scss}')
+        .pipe(sourcemaps.init())
+        .pipe(sass({}))
+        .pipe(flatten())
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest(dirs.src.css));
+});
+
+// Watch CSS task
+gulp.task('watch-css', ['watch-sass'], function () {
+    gulp
+        .src(dirs.src.css + '**/*.css')
+        .pipe(sourcemaps.init())
+        .pipe(concat("style.min.css"))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest(dirs.src.css));
+});
+
+// Watch JS task
+gulp.task('watch-js', function () {
+    return gulp
+        .src(dirs.src.js + "**/*.js")
+        .pipe(sourcemaps.init())
+        .pipe(concat('scripts.min.js'))
+        .pipe(gulp.dest(dirs.build + 'js'))
+        // .pipe(uglify())
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest(dirs.src.js));
+});
+
+// Watch Vendor JS
+gulp.task('watch-vendor-js', function () {
+    gulp
+        .src(dirs.src.vendor + "**/*.js")
+        .pipe(sourcemaps.init())
+        .pipe(concat('vendor.min.js'))
+        .pipe(uglify())
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest(dirs.src.vendor));
+});
+
+
+// Covert pug files
+gulp.task('watch-pug', function () {
+    return gulp
+        .src(dirs.src.main + '*.pug')
+        .pipe(pug())
+        .pipe(gulp.dest(dirs.src.main));
 });
